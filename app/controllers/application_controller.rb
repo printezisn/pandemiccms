5bfbd4d1ced2bbf656ab220d9cd1e534bbc105b3
Nikos <printezisn@gmail.com>
2020-12-05 19:02:11 +0200

    Add client id in devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@
 
 # Base application controller
 class ApplicationController < ActionController::Base
+  helper_method :current_client_id
+
+  def current_client_id
+    @current_client_id ||= Rails.configuration.tenants[request.domain]
+  end
 end
