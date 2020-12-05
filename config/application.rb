a8c02d42e96e7c9f57be6a9340ed090304c8410c
Nikos <printezisn@gmail.com>
2020-12-05 18:42:49 +0200

    Add config for tenants

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Pandemiccms
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.tenants = config_for(:tenants)
   end
 end
