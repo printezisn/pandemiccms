6868afeb54817a7de836b0c89cd051cfb904bc17
Nikos <printezisn@gmail.com>
2020-12-06 09:13:21 +0200

    Remove @rails/activestorage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/activestorage").start()
 require("channels")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
