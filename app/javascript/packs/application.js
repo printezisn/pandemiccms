e86e55f95faa384125268b3a1cea4d5aeff8c7b5
Nikos <printezisn@gmail.com>
2020-12-06 17:02:06 +0200

    Add main menu

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,20 @@
+const initNavbars = () => {
+  [...document.getElementsByClassName('navbar-burger')].forEach((el) => {
+    const target = document.getElementById(el.getAttribute('data-target'));
+    el.addEventListener('click', () => {
+      target.classList.toggle('is-active');
+    });
+  });
+};
+
+const init = () => {
+  initNavbars();
+};
+
+if (document.readyState === 'complete') {
+  init();
+} else {
+  document.addEventListener('DOMContentLoaded', () => {
+    init();
+  });
+}
