fec8fbc4167e5baf36c3fcea49c6a970c6b75dc1
Nikos <printezisn@gmail.com>
2020-12-08 20:38:55 +0200

    Validate admin user fields only if they have changes

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -15,12 +15,12 @@ class AdminUser < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
                     length: { maximum: 255 },
                     uniqueness: { case_sensitive: false, scope: [:client_id] },
-                    if: -> { email.present? }
+                    if: -> { email.present? && (new_record? || will_save_change_to_email?) }
   validates :username, presence: true
   validates :username, format: { with: /\A[A-Za-z0-9.\-]*\z/ },
                        length: { maximum: 50 },
                        uniqueness: { case_sensitive: false, scope: [:client_id] },
-                       if: -> { username.present? }
+                       if: -> { username.present? && (new_record? || will_save_change_to_username?) }
   validates :password, presence: true, if: -> { new_record? || password_confirmation.present? }
   validates :password, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}\z/ },
                        length: { maximum: 128 },
