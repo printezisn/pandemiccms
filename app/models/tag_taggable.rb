117d4d4c2af02ad7d2bad9c5890c8c523bd6e85e
Nikos <printezisn@gmail.com>
2021-01-08 15:49:54 +0200

    Add callbacks to update tag counters

--- app/models/tag_taggable.rb
+++ app/models/tag_taggable.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
+# TagTaggable model
 class TagTaggable < ApplicationRecord
   belongs_to :tag, inverse_of: :tag_taggables
   belongs_to :taggable, polymorphic: true
 
+  after_destroy :update_tag_counters
+  after_save :update_tag_counters
+
   scope :post, -> { where(taggable_type: 'Post') }
   scope :page, -> { where(taggable_type: 'Page') }
+
+  private
+
+  def update_tag_counters
+    tag.update!(
+      posts_count: tag.tag_taggables.post.count,
+      pages_count: tag.tag_taggables.page.count
+    )
+  end
 end
