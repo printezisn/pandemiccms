3754b29a549dff3d359037f8e8e83f9733534e51
Nikos <printezisn@gmail.com>
2021-02-26 20:38:15 +0200

    Remove counter updates in tags

--- app/models/tag_taggable.rb
+++ app/models/tag_taggable.rb
@@ -5,11 +5,6 @@ class TagTaggable < ApplicationRecord
   belongs_to :tag, inverse_of: :tag_taggables
   belongs_to :taggable, polymorphic: true
 
-  after_create :increase_posts_count, if: :post?
-  after_create :increase_pages_count, if: :page?
-  after_destroy :decrease_posts_count, if: :post?
-  after_destroy :decrease_pages_count, if: :page?
-
   scope :post, -> { where(taggable_type: 'Post') }
   scope :page, -> { where(taggable_type: 'Page') }
 
@@ -21,21 +16,5 @@ class TagTaggable < ApplicationRecord
 
   def page?
     taggable_type == 'Page'
-  end
-
-  def increase_posts_count
-    tag.increment!(:posts_count)
-  end
-
-  def decrease_posts_count
-    Tag.find_by(id: tag_id)&.decrement!(:posts_count)
-  end
-
-  def increase_pages_count
-    tag.increment!(:pages_count)
-  end
-
-  def decrease_pages_count
-    Tag.find_by(id: tag_id)&.decrement!(:pages_count)
   end
 end
--- spec/models/tag_taggable_spec.rb
+++ spec/models/tag_taggable_spec.rb
@@ -16,36 +16,4 @@ RSpec.describe TagTaggable, type: :model do
     it { expect(described_class.post.map(&:taggable)).to contain_exactly(post) }
     it { expect(described_class.page.map(&:taggable)).to contain_exactly(page) }
   end
-
-  describe '#update_tag_counters' do
-    let(:tag) { FactoryBot.create(:tag) }
-
-    context 'when post taggable is saved' do
-      it 'updates the posts_count counter' do
-        expect { FactoryBot.create(:post, tags: [tag]) }.to change(tag, :posts_count).by(1)
-      end
-    end
-
-    context 'when page taggable is saved' do
-      it 'updates the pages_count counter' do
-        expect { FactoryBot.create(:page, tags: [tag]) }.to change(tag, :pages_count).by(1)
-      end
-    end
-
-    context 'when post taggable is destroyed' do
-      let!(:post) { FactoryBot.create(:post, tags: [tag]) }
-
-      it 'updates the posts_count counter' do
-        expect { post.destroy }.to change { tag.reload.posts_count }.by(-1)
-      end
-    end
-
-    context 'when page taggable is destroyed' do
-      let!(:page) { FactoryBot.create(:page, tags: [tag]) }
-
-      it 'updates the pages_count counter' do
-        expect { page.destroy }.to change { tag.reload.pages_count }.by(-1)
-      end
-    end
-  end
 end
