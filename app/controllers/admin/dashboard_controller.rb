fe3d59f01d84daf5fe2a819867314c7898648db1
Nikos <printezisn@gmail.com>
2021-02-28 12:36:35 +0200

    Display categories count in dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,6 +6,7 @@ module Admin
     def index
       @media_count = Medium.where(client_id: current_client.id).count
       @tags_count = Tag.where(client_id: current_client.id).count
+      @categories_count = Category.where(client_id: current_client.id).count
       @users_count = AdminUser.where(client_id: current_client.id, status: :active).count if current_admin_user.supervisor?
     end
   end
