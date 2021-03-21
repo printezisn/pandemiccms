4c1a401b22bd2da287140006b14afbc2cac4a908
Nikos <printezisn@gmail.com>
2021-03-21 14:35:22 +0200

    Show my page drafts in dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,6 +8,8 @@ module Admin
       @tags_count = Tag.where(client_id: current_client.id).count
       @categories_count = Category.where(client_id: current_client.id).count
       @users_count = AdminUser.where(client_id: current_client.id, status: :active).count if current_admin_user.supervisor?
+      @pages_count = Page.where(client_id: current_client.id).count
+      @draft_pages_count = Page.draft.where(author_id: current_admin_user.id).count
     end
   end
 end
