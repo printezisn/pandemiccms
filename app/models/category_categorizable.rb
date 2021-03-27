bd82f17e72395c30d281343deddf946476511244
Nikos <printezisn@gmail.com>
2021-03-27 17:45:47 +0200

    Add basic scopes and update counters in CategoryCategorizable model

--- app/models/category_categorizable.rb
+++ app/models/category_categorizable.rb
@@ -4,4 +4,23 @@
 class CategoryCategorizable < ApplicationRecord
   belongs_to :category, inverse_of: :category_categorizables
   belongs_to :categorizable, polymorphic: true
+
+  after_create :increment_posts_count
+  before_destroy :decrement_posts_count
+
+  scope :post, -> { where(categorizable_type: 'Post') }
+
+  private
+
+  def increment_posts_count
+    category.increment!(:posts_count) if post? && categorizable&.visible?
+  end
+
+  def decrement_posts_count
+    category.decrement!(:posts_count) if post? && categorizable&.visible?
+  end
+
+  def post?
+    categorizable_type == 'Post'
+  end
 end
--- spec/models/category_categorizable_spec.rb
+++ spec/models/category_categorizable_spec.rb
@@ -3,8 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe CategoryCategorizable, type: :model do
-  subject { FactoryBot.build(:category_categorizable) }
+  subject(:model) { FactoryBot.build(:category_categorizable) }
 
   it { is_expected.to belong_to(:category) }
   it { is_expected.to belong_to(:categorizable) }
+
+  describe '#increment_posts_count' do
+    subject { category.posts_count }
+
+    let(:visibility) { :public }
+    let(:category) { FactoryBot.create(:category) }
+
+    before do
+      FactoryBot.create(:post, categories: [category], status: :published, visibility: visibility)
+    end
+
+    context 'when the post is visible' do
+      it { is_expected.to eq(1) }
+    end
+
+    context 'when the post is not visible' do
+      let(:visibility) { :private }
+
+      it { is_expected.to be_zero }
+    end
+  end
+
+  describe 'scopes' do
+    let(:category) { FactoryBot.create(:category) }
+    let!(:post) { FactoryBot.create(:post, categories: [category]) }
+
+    it { expect(described_class.post.map(&:categorizable)).to contain_exactly(post) }
+  end
 end
