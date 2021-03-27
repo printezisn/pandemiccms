2d6b848253a0e38453b788322ff51b9efce79088
Nikos <printezisn@gmail.com>
2021-03-27 19:25:38 +0200

    Add admin posts controller

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -5,23 +5,154 @@ module Admin
   class PostsController < BaseController
     PAGE_SIZE = 10
 
+    before_action :fetch_post, only: %i[show edit update destroy translate save_translation publish]
+    before_action :fetch_translation, only: %i[translate save_translation]
+    before_action :fetch_templates, only: %i[new edit create update]
+    before_action :fetch_visibilities, only: %i[new edit create update]
+
     # GET /posts
+    # GET /category/:category_id/posts
     # GET /tag/:tag_id/posts
     # GET /user/:user_id/posts
-    # GET /category/:category_id/posts
     def index
       @posts = Post.where(client_id: current_client.id)
+      @posts = @posts.where(status: params[:status]) if params[:status].present?
       @posts = @posts.joins(:tag_taggables).where(tag_taggables: { tag_id: params[:tag_id] }) if params[:tag_id].present?
       if params[:category_id].present?
         @posts = @posts.joins(:category_categorizables).where(category_categorizables: { category_id: params[:category_id] })
       end
-      # @posts = @posts.where(creator_id: params[:user_id]) if params[:user_id].present?
+      @posts = @posts.where(author_id: params[:user_id]) if params[:user_id].present?
 
       @posts = @posts.simple_text_search(params[:search])
                      .bound_sort(params[:sort_by], params[:dir])
                      .page(params[:page].to_i)
                      .per(PAGE_SIZE)
       render :_posts_table, layout: nil if request.xhr?
+    end
+
+    # GET /posts/search
+    # GET /posts/search.json
+    def search
+      posts = Post.where(client_id: current_client.id)
+                  .simple_text_search(params[:term])
+                  .bound_sort(params[:sort_by] || 'name', params[:dir])
+                  .select(:id, :name)
+                  .page(params[:page].to_i)
+                  .per(PAGE_SIZE)
+      results = posts.map { |post| { id: post.id, text: post.name } }
+      more = !posts.last_page? && results.any?
+
+      render json: { results: results, pagination: { more: more } }
+    end
+
+    # GET /posts/1/show
+    # GET /posts/1/show.json
+    def show; end
+
+    # GET /posts/1/edit
+    # GET /posts/1/edit.json
+    def edit; end
+
+    # GET /posts/new
+    def new
+      @post = Post.new
+    end
+
+    # POST /posts
+    # POST /posts.json
+    def create
+      @post = Post.new(post_params)
+      @post.client_id = current_client.id
+      @post.author_id = current_admin_user.id
+      @post.should_save_categories = true
+      @post.should_save_tags = true
+
+      result = params[:publish].present? ? @post.publish_now : @post.save
+
+      if result
+        redirect_to admin_post_path(@post), notice: _('The post was successfully created.')
+      else
+        render :new
+      end
+    end
+
+    # PATCH/PUT /posts
+    # PATCH/PUT /posts.json
+    def update
+      @post.assign_attributes(post_params)
+      @post.should_save_categories = true
+      @post.should_save_tags = true
+
+      result = params[:publish].present? ? @post.publish_now : @post.save
+
+      if result
+        redirect_to admin_post_path(@post), notice: _('The post was successfully updated.')
+      else
+        render :edit
+      end
+    end
+
+    # DELETE /posts/1
+    # DELETE /posts/1.json
+    def destroy
+      @post.destroy
+
+      redirect_to admin_posts_path, notice: _('The post was successfully deleted.')
+    end
+
+    # POST /posts/1/publish
+    # POST /posts/2/publish.json
+    def publish
+      if @post.publish_now
+        redirect_to admin_post_path(@post), notice: _('The post was successfully published.')
+      else
+        redirect_to admin_post_path(@post), alert: _('The post could not be published.')
+      end
+    end
+
+    # GET /posts/1/translate
+    def translate; end
+
+    # POST /posts/1/translate
+    def save_translation
+      @post.assign_attributes(translation_params)
+
+      if @post.save_translation(translation_locale)
+        redirect_to translate_admin_post_path(@post, translation_locale: translation_locale),
+                    notice: _('The post was successfully translated.')
+      else
+        @translation.assign_attributes(translation_params)
+
+        render :translate
+      end
+    end
+
+    private
+
+    def fetch_post
+      @post = Post.find_by!(id: params[:id], client_id: current_client.id)
+    end
+
+    def fetch_translation
+      @translation = @post.translate(translation_locale)
+    end
+
+    def fetch_templates
+      path = Rails.root.join("app/views/templates/#{current_client.template}/posts/templates/*.html.erb")
+      @templates = Dir[path].map { |f| File.basename(f, '.html.erb') }.sort
+    end
+
+    def fetch_visibilities
+      @visibilities = [[_('Public|Visibility'), :public], [_('Private|Visibility'), :private]]
+    end
+
+    def post_params
+      params.require(:post).permit(:image, :should_remove_image, :name, :slug, :description, :body,
+                                   :template, category_names: [], tag_names: [])
+    end
+
+    def translation_params
+      params.require(:post).permit(Post::TRANSLATABLE_FIELDS)
     end
   end
 end
---
+++ spec/requests/admin/posts_spec.rb
@@ -0,0 +1,301 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './spec/requests/admin/shared/access'
+
+RSpec.describe '/admin/posts', type: :request do
+  let!(:admin_user) { FactoryBot.create(:admin_user) }
+  let(:signed_in_user) { admin_user }
+
+  let(:model) { FactoryBot.build(:post, author_id: admin_user.id) }
+
+  before do
+    sign_in signed_in_user if signed_in_user
+  end
+
+  describe 'GET /index' do
+    let(:request) { get admin_posts_path }
+
+    before { model.save! }
+
+    it_behaves_like 'admin user page'
+
+    it 'returns a successful response' do
+      request
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    let(:request) { get admin_post_path(model) }
+
+    before { model.save! }
+
+    it_behaves_like 'admin user page'
+
+    it 'returns a successful response' do
+      request
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /new' do
+    let(:request) { get new_admin_post_path }
+
+    it_behaves_like 'admin user page'
+
+    it 'returns a successful response' do
+      request
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /edit' do
+    let(:request) { get edit_admin_post_path(model) }
+
+    before { model.save! }
+
+    it_behaves_like 'admin user page'
+
+    it 'returns a successful response' do
+      request
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    let(:post_params) do
+      {
+        image: Rack::Test::UploadedFile.new(File.open(Rails.root.join('spec/fixtures/test.png'))),
+        name: model.name,
+        slug: model.slug,
+        description: model.description,
+        body: model.body,
+        template: model.template,
+        visibility: :private
+      }
+    end
+
+    let(:request) { post admin_posts_path, params: { post: post_params } }
+
+    it_behaves_like 'admin user page'
+
+    context 'with valid parameters' do
+      it 'creates a new post' do
+        expect { request }.to change(Post, :count).by(1)
+      end
+
+      it 'redirects to the created post' do
+        request
+
+        expect(response).to redirect_to(admin_post_path(id: Post.last.id, locale: 'en'))
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:post_params) do
+        {
+          image: Rack::Test::UploadedFile.new(File.open(Rails.root.join('spec/fixtures/test.png'))),
+          name: '',
+          slug: model.slug,
+          description: model.description,
+          body: model.body,
+          template: model.template,
+          visibility: :private
+        }
+      end
+
+      it 'does not create a new post' do
+        expect { request }.not_to change(Post, :count)
+      end
+
+      it 'returns a successful response' do
+        request
+
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    let(:post_params) do
+      {
+        image: Rack::Test::UploadedFile.new(File.open(Rails.root.join('spec/fixtures/test.png'))),
+        name: model.name,
+        slug: model.slug,
+        description: 'New description',
+        body: model.body,
+        template: model.template,
+        visibility: :private
+      }
+    end
+
+    let(:request) { patch admin_post_path(model), params: { post: post_params } }
+
+    before { model.save! }
+
+    it_behaves_like 'admin user page'
+
+    context 'with valid parameters' do
+      it 'updates the requested post' do
+        request
+
+        expect(model.reload.description).to eq(post_params[:description])
+      end
+
+      it 'redirects to the post' do
+        request
+
+        expect(response).to redirect_to(admin_post_path(id: model.id, locale: 'en'))
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:post_params) do
+        {
+          name: '',
+          slug: model.slug,
+          description: 'New description',
+          body: model.body,
+          template: model.template,
+          visibility: :private
+        }
+      end
+
+      it 'returns a successful response' do
+        request
+
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    let(:request) { delete admin_post_path(model) }
+
+    before { model.save! }
+
+    it_behaves_like 'admin user page'
+
+    it 'destroys the requested post' do
+      expect { request }.to change(Post, :count).by(-1)
+    end
+
+    it 'redirects to the posts list' do
+      request
+
+      expect(response).to redirect_to(admin_posts_path(locale: 'en'))
+    end
+  end
+
+  describe 'GET /translate' do
+    let(:request) { get translate_admin_post_path(model) }
+
+    before { model.save! }
+
+    it_behaves_like 'admin user page'
+
+    it 'returns a successful response' do
+      request
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /translate' do
+    let(:translation_params) do
+      {
+        'name' => 'Translated Name',
+        'slug' => 'translated-slug',
+        'description' => 'Translated description',
+        'body' => 'Translated Body'
+      }
+    end
+
+    let(:request) do
+      post translate_admin_post_path(model), params: { translation_locale: 'en', post: translation_params }
+    end
+
+    before { model.save! }
+
+    it_behaves_like 'admin user page'
+
+    context 'with valid parameters' do
+      it 'translates the requested post' do
+        request
+
+        expect(model.reload.translate(:en).attributes.slice(*translation_params.keys)).to eq(translation_params)
+      end
+
+      it 'redirects to the post translation' do
+        request
+
+        expect(response).to redirect_to(translate_admin_post_path(id: model.id, locale: 'en', translation_locale: 'en'))
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:translation_params) do
+        {
+          'name' => Array.new(256) { 'a' }.join,
+          'slug' => 'translated-slug',
+          'description' => 'Translated description',
+          'body' => 'Translated body'
+        }
+      end
+
+      it 'returns a successful response' do
+        request
+
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'GET /search' do
+    let(:request) { get search_admin_posts_path(term: model.name) }
+    let(:expected_results) do
+      {
+        'results' => [{ 'id' => model.id, 'text' => model.name }],
+        'pagination' => { 'more' => false }
+      }
+    end
+    let(:model) { FactoryBot.create(:post) }
+
+    it_behaves_like 'admin user page'
+
+    it 'returns a successful response' do
+      request
+
+      expect(response).to be_successful
+    end
+
+    it 'returns the correct posts' do
+      request
+
+      expect(JSON.parse(response.body)).to eq(expected_results)
+    end
+  end
+
+  describe 'POST /publish' do
+    let(:request) { post publish_admin_post_path(model) }
+
+    let(:model) { FactoryBot.create(:post) }
+
+    it_behaves_like 'admin user page'
+
+    it 'redirects to the post' do
+      request
+
+      expect(response).to redirect_to(admin_post_path(id: model.id, locale: 'en'))
+    end
+
+    it 'changes the post status' do
+      expect { request }.to change { model.reload.status }.from('draft').to('published')
+    end
+  end
+end
