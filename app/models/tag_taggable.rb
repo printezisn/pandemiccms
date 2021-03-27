f58cf7fba52933872730c71a7dd33aa06dd6ab17
Nikos <printezisn@gmail.com>
2021-03-27 17:31:13 +0200

    Update post counter in TagTaggable model

--- app/models/tag_taggable.rb
+++ app/models/tag_taggable.rb
@@ -5,10 +5,21 @@ class TagTaggable < ApplicationRecord
   belongs_to :tag, inverse_of: :tag_taggables
   belongs_to :taggable, polymorphic: true
 
+  after_create :increment_posts_count
+  before_destroy :decrement_posts_count
+
   scope :post, -> { where(taggable_type: 'Post') }
   scope :page, -> { where(taggable_type: 'Page') }
 
   private
+
+  def increment_posts_count
+    tag.increment!(:posts_count) if post? && taggable&.visible?
+  end
+
+  def decrement_posts_count
+    tag.decrement!(:posts_count) if post? && taggable&.visible?
+  end
 
   def post?
     taggable_type == 'Post'
--- spec/models/tag_taggable_spec.rb
+++ spec/models/tag_taggable_spec.rb
@@ -3,10 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe TagTaggable, type: :model do
-  subject { FactoryBot.build(:tag_taggable) }
+  subject(:model) { FactoryBot.build(:tag_taggable) }
 
   it { is_expected.to belong_to(:tag) }
   it { is_expected.to belong_to(:taggable) }
+
+  describe '#increment_posts_count' do
+    subject { tag.posts_count }
+
+    let(:visibility) { :public }
+    let(:tag) { FactoryBot.create(:tag) }
+
+    before do
+      FactoryBot.create(:post, tags: [tag], status: :published, visibility: visibility)
+      FactoryBot.create(:page, tags: [tag])
+    end
+
+    context 'when the post is visible' do
+      it { is_expected.to eq(1) }
+    end
+
+    context 'when the post is not visible' do
+      let(:visibility) { :private }
+
+      it { is_expected.to be_zero }
+    end
+  end
 
   describe 'scopes' do
     let(:tag) { FactoryBot.create(:tag) }
