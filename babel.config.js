87e0408bc9decfb759299d0b425dbdf64150ee83
Nikos <printezisn@gmail.com>
2021-05-09 00:36:52 +0300

    Fix issues with webpack bundle build

--- babel.config.js
+++ babel.config.js
@@ -45,6 +45,18 @@ module.exports = function(api) {
         '@babel/plugin-proposal-class-properties',
         {
           loose: true
+        },
+      ],
+      [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true
         }
       ],
       [
