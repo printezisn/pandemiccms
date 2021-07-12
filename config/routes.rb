8dda29927b47fc08180ee22f1fb1ee1c78d7a788
Nikos <printezisn@gmail.com>
2021-07-12 21:15:06 +0300

    Enable recurring job to index unindexed entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
   Sidekiq::Web.use Rack::Auth::Basic, 'Protected Area' do |username, password|
