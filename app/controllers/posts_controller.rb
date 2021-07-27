cd7c225941f0e63657c55cff113e5f069b73b445
Nikos <printezisn@gmail.com>
2021-07-27 10:51:25 +0300

    Load posts in themes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,16 @@
 class PostsController < ApplicationController
   # GET /p/1/slug
   def show
-    render "templates/#{current_client.template}/posts/default"
+    @model = CacheFetcher.call(current_client, current_cache_version, ['post', params[:id]]) do
+      Post.includes(:translations).find_by!(client_id: current_client.id, id: params[:id])
+    end
+    raise ActionController::RoutingError, 'Not Found' if !@model.visible? && current_admin_user.nil?
+
+    @translated_model = @model.translate(current_locale, use_defaults: true)
+
+    slug = @translated_model.displayed_slug(current_language&.transliterations)
+    return redirect_to post_path(id: @model.id, slug: slug) if slug != params[:slug]
+
+    render "templates/#{current_client.template}/posts/#{@model.template}"
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,12 +3,55 @@
 require 'rails_helper'
 
 RSpec.describe 'posts', type: :request do
-  before { FactoryBot.create(:client) }
+  subject!(:model) do
+    FactoryBot.create(
+      :post,
+      template: template,
+      status: status,
+      visibility: visibility
+    )
+  end
+
+  let(:template) { 'default' }
+  let(:status) { 'published' }
+  let(:visibility) { 'public' }
 
   describe 'GET /show' do
-    it 'is successful' do
-      get post_path(id: 1, slug: 'test')
-      expect(response).to be_successful
+    let(:request) { get post_path(id: model.id, slug: slug) }
+    let(:slug) { model.displayed_slug(nil) }
+
+    context 'when the post is private' do
+      let(:visibility) { 'private' }
+
+      it 'raises a routing error' do
+        expect { request }.to raise_error(ActionController::RoutingError)
+      end
+    end
+
+    context 'when the post is draft' do
+      let(:status) { 'draft' }
+
+      it 'raises a routing error' do
+        expect { request }.to raise_error(ActionController::RoutingError)
+      end
+    end
+
+    context 'when the post is visible' do
+      it 'is successful' do
+        request
+
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when the slug is different' do
+      let(:request) { get post_path(id: model.id, slug: 'different-slug') }
+
+      it 'redirects to the correct slug' do
+        request
+
+        expect(response).to redirect_to(post_path(id: model.id, slug: slug))
+      end
     end
   end
 end
