fe9cf38dd2ec2ab03717fd518ab93b8c79bb226a
Nikos <printezisn@gmail.com>
2021-07-28 15:37:31 +0300

    Add meta for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,7 +4,7 @@
 class TagsController < ApplicationController
   # GET /t/1/slug
   def show
-    @model = Tag.includes(:translations).find_by!(client_id: current_client.id, id: params[:id])
+    @model = Tag.includes(:translations).find_by!(client_id: current_client.id, id: params[:id]).decorate
     raise ActionController::RoutingError, 'Not Found' if @model.private_visibility? && current_admin_user.nil?
 
     @translated_model = @model.translate(current_locale, use_defaults: true)
@@ -12,6 +12,6 @@ class TagsController < ApplicationController
     slug = @translated_model.displayed_slug(current_language&.transliterations)
     return redirect_to tag_path(id: @model.id, slug: slug), status: :moved_permanently if slug != params[:slug]
 
-    render "templates/#{current_client.template}/tags/#{@model.template}"
+    render "templates/#{current_client.template}/tags/#{@model.template.presence || 'default'}"
   end
 end
