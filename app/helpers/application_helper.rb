706f9c8c5d866251a72ece70a3fa579cdb4fc0f4
Nikos <printezisn@gmail.com>
2021-10-10 17:55:30 +0300

    Make request base url part of client cache key

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,14 @@ module ApplicationHelper
   def client_cache(key, &block)
     cache_if(
       current_client.cache_enabled?,
-      [current_client.id, current_cache_version, current_locale, admin_user_signed_in?, key],
+      [
+        request.base_url,
+        current_client.id,
+        current_cache_version,
+        current_locale,
+        admin_user_signed_in?,
+        key
+      ],
       expires_in: current_client.cache_duration&.minutes,
       race_condition_ttl: 10.seconds,
       &block
