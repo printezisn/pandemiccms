3e819136431eaf035c857049d9b03939b54abe7c
Nikos <printezisn@gmail.com>
2022-01-25 19:30:24 +0200

    Upgrade to Rails 7

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,15 @@ module Pandemiccms
   # Base application config
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.exceptions_app = routes
 
@@ -20,10 +28,5 @@ module Pandemiccms
       { locale: 'en-GB', name: 'English', flag: 'gb' },
       { locale: 'el-GR', name: 'Ελληνικά', flag: 'gr', transliterations: 'greek' }
     ]
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
