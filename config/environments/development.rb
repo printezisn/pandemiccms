1efd224c14b6b1cadd6888466ede2a6b054303f9
printezisn <printezisn@gmail.com>
2024-05-05 12:30:12 +0300

    Install solid queue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_adapter = :solid_queue
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_adapter = :solid_queue
   # config.active_job.queue_name_prefix = "pandemiccms_production"
 
   config.action_mailer.perform_caching = false
